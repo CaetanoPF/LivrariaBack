0765e597d127cc8327eeb8f212773c28c24a31e9
CaetanoPF <caetanopfreitas@gmail.com>
2025-05-16 08:38:12 -0300

    feat: Criação de dois serializadores para Livro

--- core/serializers/livro.py
+++ core/serializers/livro.py
@@ -1,3 +1,4 @@
+from os import defpath
 from rest_framework.serializers import ModelSerializer
 
 from core.models import Livro
@@ -6,3 +7,10 @@ class LivroSerializer(ModelSerializer):
     class Meta:
         model = Livro
         fields = "__all__"
+
+
+class LivroListRetrieveSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = "__all__"
+        depth = 1
