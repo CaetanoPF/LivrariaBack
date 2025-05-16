340fed61df6a9f404b57ce9c41205e1a48b61e63
CaetanoPF <caetanopfreitas@gmail.com>
2025-05-16 08:50:34 -0300

    feat: Criação de múltiplos serializadores para Livro

--- core/serializers/livro.py
+++ core/serializers/livro.py
@@ -3,13 +3,18 @@ from rest_framework.serializers import ModelSerializer
 
 from core.models import Livro
 
+
 class LivroSerializer(ModelSerializer):
     class Meta:
         model = Livro
         fields = "__all__"
 
+class LivroListSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = ("id", "titulo", "preco")
 
-class LivroListRetrieveSerializer(ModelSerializer):
+class LivroRetrieveSerializer(ModelSerializer):
     class Meta:
         model = Livro
         fields = "__all__"
