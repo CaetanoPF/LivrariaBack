9da27f4b882c437a34667d1ba0851c15bd89da6e
CaetanoPF <caetanopfreitas@gmail.com>
2025-06-06 09:23:50 -0300

    fix: Upload dos livros

--- core/models/livro.py
+++ core/models/livro.py
@@ -14,7 +14,8 @@ class Livro(models.Model):
         Categoria, on_delete=models.PROTECT, related_name="livros", null=True, blank=True
     )
     editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name="livros", null=True, blank=True)
-    autor = models.ForeignKey(Autor, on_delete=models.PROTECT, related_name='livros', null=True, blank=True)
+    autores = models.ManyToManyField(Autor, related_name='livros', blank=True)
+
     capa = models.ForeignKey(
     Image,
     related_name="+",
