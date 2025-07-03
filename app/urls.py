0dd30fbafc8ab219bb92a9fca0d9783f0694dd05
CaetanoPF <caetanopfreitas@gmail.com>
2025-07-03 10:59:47 -0300

    feat: criação do endpoint para a listagem básica de compras

--- app/urls.py
+++ app/urls.py
@@ -10,7 +10,7 @@ from drf_spectacular.views import (
 )
 from rest_framework.routers import DefaultRouter
 
-from core.views import EditoraViewSet, UserViewSet, CategoriaViewSet, AutorViewSet, LivroViewSet
+from core.views import EditoraViewSet, UserViewSet, CategoriaViewSet, AutorViewSet, LivroViewSet, CompraViewSet
 
 router = DefaultRouter()
 router.register(r'usuarios', UserViewSet, basename='usuarios')
@@ -18,6 +18,7 @@ router.register(r'categorias', CategoriaViewSet, basename='categorias')
 router.register(r'editoras', EditoraViewSet, basename='editoras')
 router.register(r'autores', AutorViewSet, basename='autores')
 router.register(r'livros', LivroViewSet, basename='livros')
+router.register(r'compras', CompraViewSet, basename='compras')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
